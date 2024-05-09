3792c2a18731b80e0d4b470705cc80684d3aa08a
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-09 07:44:22 -0300

    feat(hooks): Add useImages hook

---
+++ src/hooks/useImages.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+
+export type ImageDetails = {
+  key: string,
+  path: string,
+}
+
+export type ImageTable = {
+  [key: string]: ImageBitmap
+}
+
+async function loadImage(path: string) {
+  const response = await fetch(path);
+  const blob = await response.blob();
+  return await createImageBitmap(blob);
+}
+
+export function useImages(paths: ImageDetails[]): ImageTable | null {
+  const [images, setImages] = useState<ImageTable | null>(null);
+
+  useEffect(() => {
+    async function loadImages() {
+      const output: ImageTable = {};
+      for (const { key, path } of paths) {
+        output[key] = await loadImage(path);
+      }
+      setImages(output);
+    }
+    loadImages();
+  }, [paths]);
+
+  return images;
+}
