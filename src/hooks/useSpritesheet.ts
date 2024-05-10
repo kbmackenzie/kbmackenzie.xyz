3481b14b54c026ed9114ebfa5558dfafcc4a2463
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-10 06:52:10 -0300

    refactor(hooks): Add better type names in useSpritesheet

--- src/hooks/useSpritesheet.ts
+++ src/hooks/useSpritesheet.ts
@@ -1,40 +1,40 @@
 import { useState, useEffect } from 'react';
 
-export type Rect = {
+export type Rectangle = {
   x: number;
   y: number;
   width: number;
   height: number;
 };
 
-export type Sprite = {
+export type SpriteDetails = {
   key: string;
-  rect: Rect;
+  rect: Rectangle;
 };
 
 export type SpriteTable = {
   [key: string]: ImageBitmap;
 };
 
-async function loadSprites(imagePath: string, spriteRects: Sprite[]): Promise<SpriteTable> {
+async function loadSprites(imagePath: string, spriteDetails: SpriteDetails[]): Promise<SpriteTable> {
   const response = await fetch(imagePath);
   const blob = await response.blob();
 
   const table: SpriteTable = {};
 
-  for (const { key, rect } of spriteRects) {
+  for (const { key, rect } of spriteDetails) {
     table[key] = await createImageBitmap(blob, rect.x, rect.y, rect.width, rect.height);
   }
   return table;
 }
 
-export function useSpritesheet(imagePath: string, spriteRects: Sprite[]): SpriteTable | null {
+export function useSpritesheet(imagePath: string, spriteDetails: SpriteDetails[]): SpriteTable | null {
   const [sprites, setSprites] = useState<SpriteTable | null>(null);
 
   useEffect(() => {
-    loadSprites(imagePath, spriteRects)
+    loadSprites(imagePath, spriteDetails)
       .then(table => setSprites(table));
-  }, [imagePath, spriteRects]);
+  }, [imagePath, spriteDetails]);
 
   return sprites;
 }
