d558361a6ee47dcd003d4ecc6837859d90f8f355
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-24 06:23:37 -0300

    chore: Add custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      night:      '#160924',
+      moon:       '#231136',
+      space:      '#10061a',
+      hotpink:    '#ef476f',
+      bubblegum:  '#ca416d',
+    },
     extend: {},
   },
   plugins: [],
