33e3bae7045d203c000a0fd09f1ac338eb2c3434
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-24 22:24:27 -0300

    feat(pages): Add background color to Header component

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -2,7 +2,7 @@ import AlpacaAnimation from "@components/AlpacaAnimation"
 
 export default function Header() {
   return (
-    <header className="grid grid-cols-2 sm:grid-cols-3 items-end min-h-32">
+    <header className="grid grid-cols-2 sm:grid-cols-3 items-end min-h-32 bg-space">
       <h1 className="col-span-2 text-night font-firamono text-6xl">
         kbmackenzie
       </h1>
