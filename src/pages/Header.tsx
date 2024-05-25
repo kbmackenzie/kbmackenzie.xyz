db963914e630d9aa5bc0a3fcf86b4f4dcb114443
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-24 21:20:10 -0300

    feat(pages): Add better heading in Header component

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -3,7 +3,9 @@ import AlpacaAnimation from "@components/AlpacaAnimation"
 export default function Header() {
   return (
     <header className="grid grid-cols-2 sm:grid-cols-3 items-end min-h-32">
-      <h1 className="text-white col-span-2">kbmackenzie</h1>
+      <h1 className="col-span-2 text-night font-firamono text-6xl">
+        kbmackenzie
+      </h1>
       <AlpacaAnimation className="hidden sm:block max-w-full max-h-full" />
     </header>
   )
