f53034c1541a58485119a26147b7241174007a89
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-28 16:44:03 -0300

    feat(components): Title should be a string

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,11 @@
-import kbm from '@assets/kbm.webp';
+import '@components/Title.css';
 
 export default function Title({...props}) {
   return (
-    <img src={kbm} alt='kbm logo' {...props}/>
+    <h1 {...props}>
+      <span className="text-bubblegum">echo </span>
+      <span className="text-white">kbmackenzie</span>
+    </h1>
+    //<img src={kbm} alt='kbm logo' {...props}/>
   );
 }
