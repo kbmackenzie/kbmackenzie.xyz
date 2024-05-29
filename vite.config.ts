ec4972ad2c07d34aabd627e0aa6f7f14db448bab
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-29 04:45:32 -0300

    chore: Add Vite path aliases, add @types/node as dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = dirname(__filename);
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@style': resolve(__dirname, 'src/style'),
+    },
+  },
   plugins: [react(), tsconfigPaths()],
 })
