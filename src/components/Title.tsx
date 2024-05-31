7aef51e3cb1803990163bbf5f16612d23468701a
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-31 08:49:08 -0300

    feat(components): Add typing effect for title heading

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,8 +4,10 @@ export default function Title({...props}) {
   return (
     <h1 {...props}>
       <span className="fira-mono title">
-        <span className="title-echo">echo </span>
-        <span className="title-name">kbmackenzie</span>
+        <span className="title-type">
+          <span className="title-type-echo">$ echo </span>
+          <span className="title-type-name">kbmackenzie</span>
+        </span>
       </span>
     </h1>
   );
