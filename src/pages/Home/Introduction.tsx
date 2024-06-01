79ec153e3933e64fe4942a437ff0dd2f9bc16ff8
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-01 15:44:06 -0300

    feat(pages): Begin work on introduction page

---
+++ src/pages/Home/Introduction.tsx
@@ -0,0 +1,9 @@
+import '@pages/Home/Introduction.sass';
+
+export default function Introduction() {
+  return (
+    <section className="bg-moon-light home-intro">
+      Hello world!
+    </section>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import PageBase from "@components/PageBase";
+import '@pages/Home/index.sass';
+import Introduction from "@pages/Home/Introduction";
 
 export default function Home() {
   return (
     <PageBase>
-
+      <div className="home">
+        <Introduction />
+      </div>
     </PageBase>
   )
 }
