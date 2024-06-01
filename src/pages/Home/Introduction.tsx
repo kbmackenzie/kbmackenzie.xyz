d2d84838cd77eff9872a9ca299decb6f25e410f6
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-01 18:22:04 -0300

    feat(pages): Add introducion section text and layout

--- src/pages/Home/Introduction.tsx
+++ src/pages/Home/Introduction.tsx
@@ -1,9 +1,21 @@
 import '@pages/Home/Introduction.sass';
 
+function Description({...props}) {
+  return (
+    <section {...props}>
+      <h1 className="fira-mono greeting">
+        <span className="greeting-type">Hi, I'm Kelly!</span>
+      </h1>
+      <p>Hi, I’m Kelly! I’m a full-stack web developer, language tinkerer and hobbyist game developer. I’m fascinated by compilers, interpreters and text parsing.</p>
+    </section>
+  );
+}
+
 export default function Introduction() {
   return (
-    <section className="bg-moon-light home-intro">
-      Hello world!
+    <section className="introduction">
+      <div className="introduction-image"></div>
+      <Description className="introduction-desc"/>
     </section>
   );
 }
