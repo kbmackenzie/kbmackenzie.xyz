05b17150f0f1840298bb9a8182feb8180d614ab4
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-02 06:03:18 -0300

    feat(pages): Add better styling for home page introduction

--- src/pages/Home/Introduction.tsx
+++ src/pages/Home/Introduction.tsx
@@ -3,9 +3,7 @@ import '@pages/Home/Introduction.sass';
 function Description({...props}) {
   return (
     <section {...props}>
-      <h1 className="fira-mono greeting">
-        <span className="greeting-type">Hi, I'm Kelly!</span>
-      </h1>
+      <h1>Hi, I'm Kelly!</h1>
       <p>Hi, I’m Kelly! I’m a full-stack web developer, language tinkerer and hobbyist game developer. I’m fascinated by compilers, interpreters and text parsing.</p>
     </section>
   );
@@ -15,7 +13,7 @@ export default function Introduction() {
   return (
     <section className="introduction">
       <div className="introduction-image"></div>
-      <Description className="introduction-desc"/>
+      <Description className="introduction-desc open-sans"/>
     </section>
   );
 }
