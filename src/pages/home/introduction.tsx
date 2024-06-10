04faea2e190c30fd5791d0f7b39e51d5f8334e1c
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-09 22:39:40 -0300

    refactor(home): Re-organize introduction section

--- src/pages/home/introduction.tsx
+++ src/pages/home/introduction.tsx
@@ -1,8 +1,14 @@
 import '@pages/home/introduction.sass';
 
-function Description({...props}) {
+function AlpacaIcon() {
   return (
-    <section {...props}>
+    <div className="alpaca-icon"></div>
+  );
+}
+
+function Description() {
+  return (
+    <section className="description open-sans">
       <h1>Hi, I'm Kelly!</h1>
       <p>Hi, I’m Kelly! I’m a full-stack web developer, language tinkerer and hobbyist game developer. I’m fascinated by compilers, interpreters and text parsing.</p>
     </section>
@@ -12,8 +18,8 @@ function Description({...props}) {
 export function Introduction() {
   return (
     <section className="introduction">
-      <div className="introduction-image"></div>
-      <Description className="introduction-desc open-sans"/>
+      <AlpacaIcon />
+      <Description />
     </section>
   );
 }
