8b3bb043598b1950f87b997784d65d6f8893e87b
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-10 05:30:00 -0300

    refactor(home): Rewrite Introduction component using TwoThirds

--- src/pages/home/components/introduction.tsx
+++ src/pages/home/components/introduction.tsx
@@ -1,25 +1,20 @@
+import { TwoThirds } from '@components/sections/two-thirds';
 import '@pages/home/components/introduction.sass';
 
 function AlpacaIcon() {
   return (
-    <div className="alpaca-icon"></div>
-  );
-}
-
-function Description() {
-  return (
-    <section className="description open-sans">
-      <h1>Hi, I'm Kelly!</h1>
-      <p>Hi, I’m Kelly! I’m a full-stack web developer, language tinkerer and hobbyist game developer. I’m fascinated by compilers, interpreters and text parsing.</p>
-    </section>
+    <div className="introduction-alpaca">
+    </div>
   );
 }
 
 export function Introduction() {
   return (
-    <section className="introduction">
-      <AlpacaIcon />
-      <Description />
-    </section>
+    <TwoThirds classes={['introduction', 'open-sans']} aside={<AlpacaIcon />}>
+      <section className="introduction-description">
+        <h1>Hi, I'm Kelly!</h1>
+        <p>Hi, I’m Kelly! I’m a full-stack web developer, language tinkerer and hobbyist game developer. I’m fascinated by compilers, interpreters and text parsing.</p>
+      </section>
+    </TwoThirds>
   );
 }
