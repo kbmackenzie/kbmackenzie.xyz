881215c04e69c244512308abd184d12d0fb9094a
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-14 04:25:43 -0300

    refactor(components): Wrap TwoColumns children in <div>, not <section>

--- src/components/sections/two-columns.tsx
+++ src/components/sections/two-columns.tsx
@@ -12,9 +12,9 @@ type Props = {
 export function TwoColumns({ aside, reverse, children, classes }: Props) {
   const twoColumns = reverse ? 'two-columns-reverse' : 'two-columns';
   return (
-    <section className={classNames(twoColumns, classes)}>
+    <div className={classNames(twoColumns, classes)}>
       <div className="two-columns-aside">{aside}</div>
       <div className="two-columns-content">{children}</div>
-    </section>
+    </div>
   );
 }
