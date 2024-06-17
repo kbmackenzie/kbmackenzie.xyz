c04afa52db53a764066c8b74021a4942f8cbd838
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-17 03:12:51 -0300

    feat(layouts): Rework navigation to support small screens

--- src/layouts/base/components/navigation.tsx
+++ src/layouts/base/components/navigation.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { NavigationButtons } from '@components/navigation/navigation-buttons';
+import { SideMenu } from '@components/navigation/side-menu';
 import { LinkData } from '@shared/link';
 import '@layouts/base/components/navigation.sass'
+import menuIcon from '@layouts/base/assets/menu-icon.svg';
 
 const buttons: LinkData[] = [
   {
@@ -20,10 +23,23 @@ const buttons: LinkData[] = [
   },
 ];
 
+function MobileNavigation() {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  return (
+    <nav className="header-navigation-mobile">
+      <button className="header-mobile-hamburger" onClick={() => setShowMenu(x => !x)}>
+        <img src={menuIcon} alt="hamburger icon"></img>
+      </button>
+      {showMenu && <SideMenu buttons={buttons} classes={['header-mobile-side-menu', 'fira-mono']}/>}
+    </nav>
+  );
+}
+
 export function Navigation() {
   return (
     <>
       <NavigationButtons buttons={buttons} classes={['header-navigation', 'fira-mono']} />
+      <MobileNavigation />
     </>
   )
 }
