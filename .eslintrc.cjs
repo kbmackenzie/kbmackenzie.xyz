9415c4f1a160102e353d877c0e8a83cc64e4b85b
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-22 02:45:14 -0300

    chore(eslint): Ignore 'postcss.config.js'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'postcss.config.js'],
   parser: '@typescript-eslint/parser',
   plugins: [
     'react-refresh',
