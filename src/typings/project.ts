bc3bf52dc8c4697b2f0b0696e911e6bb4767d710
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-26 09:41:22 -0300

    feat(typings): Make Project's 'icon' field optional

--- src/typings/project.ts
+++ src/typings/project.ts
@@ -1,8 +1,8 @@
 import { Icon } from '@components/decoration/honeycomb-grid';
 
 export type Project = {
-  name: string;
-  url:  string;
-  icon: Icon;
-  description: string;
+  name:         string;
+  url:          string;
+  icon?:        Icon;
+  description:  string;
 };
