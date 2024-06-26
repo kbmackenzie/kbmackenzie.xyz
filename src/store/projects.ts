c63158dcf07a9873b00f7c7d65241b5699fe7b9c
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-26 09:41:35 -0300

    feat(store): Add projects constant

---
+++ src/store/projects.ts
@@ -0,0 +1,18 @@
+import { Project } from '@typings/project'
+
+export const projects: Project[] = [
+  {
+    name: 'mewlix',
+    url: 'https://github.com/kbmackenzie/mewlix',
+    icon: {
+      src: 'https://github.com/kbmackenzie/mewlix/blob/main/logo.svg',
+      alt: 'mewlix logo',
+    },
+    description: 'A cat-themed scripting language for making little games.',
+  },
+  {
+    name: 'kbmackenzie.xyz',
+    url: 'https://github.com/kbmackenzie/kbmackenzie.xyz',
+    description: 'The website you\'re currently on!',
+  },
+]
