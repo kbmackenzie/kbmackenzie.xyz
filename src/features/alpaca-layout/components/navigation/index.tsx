3e927328f675659aa3d3c1e70639edc26f4903fd
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 05:42:18 -0300

    feat(alpaca-layout): Add Navigation component

---
+++ src/features/alpaca-layout/components/navigation/index.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { Buttons } from '@/features/alpaca-layout/components/buttons';
+import { SideMenu } from '@/features/alpaca-layout/components/side-menu';
+import { ButtonData } from '@/features/alpaca-layout/types/button-data';
+import styles from '@/features/alpaca-layout/components/navigation/index.module.sass';
+import menuIcon from '@/features/alpaca-layout/assets/menu-icon.svg';
+
+const buttons: ButtonData[] = [
+  {
+    type: 'regular',
+    text: 'Home',
+    url: '#',
+  },
+  {
+    type: 'regular',
+    text: 'Blog',
+    url: '#',
+  },
+  {
+    type: 'focus',
+    text: 'Projects',
+    url: '#',
+  },
+];
+
+function MobileNavigation() {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  return (
+    <nav className={styles.mobile}>
+      <button className={styles.hamburger} onClick={() => setShowMenu(x => !x)}>
+        <Image src={menuIcon} alt="hamburger icon"></Image>
+      </button>
+      {showMenu &&
+        <SideMenu
+          buttons={buttons}
+          portalTarget={'side-menu'}
+          onExit={() => { setShowMenu(false); }} />}
+    </nav>
+  );
+}
+
+export function Navigation() {
+  return (
+    <div>
+      <Buttons buttons={buttons} />
+      <MobileNavigation />
+    </div>
+  );
+}
