68d75d83d06bd4011572425bfa2005bbe65d27c3
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 06:34:36 -0300

    feat(alpaca-layout): Add 'Fira Mono' font to Title component

--- src/features/alpaca-layout/components/title/index.tsx
+++ src/features/alpaca-layout/components/title/index.tsx
@@ -1,11 +1,17 @@
-import { DOMAttributes } from 'react';
 import { PipedProps } from '@/types/piped-props';
 import styles from '@/features/alpaca-layout/components/title/index.module.sass';
 
+import { Fira_Mono } from 'next/font/google';
+const firaMono = Fira_Mono({
+  weight:  ['400', '500', '700'],
+  subsets: ['latin'],
+});
+
 export function Title({ ...props }: PipedProps<HTMLHeadingElement>) {
+  const classes = [firaMono.className, styles.title].join(' ');
   return (
     <h1 {...props}>
-      <span className={styles.title}>
+      <span className={classes}>
         <span className={styles.typing}>
           <span className={styles.echo}>$ echo </span>
           <span className={styles.name}>kbmackenzie</span>
