385b8a46c5df30f35114274d9eb88deb6d173e5f
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 03:24:56 -0300

    feat(hooks): Add useAnimation hook

---
+++ src/hooks/use-animation.ts
@@ -0,0 +1,24 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export type AnimationCallback = (delta: number) => void;
+
+export function useAnimation(callback: AnimationCallback): void {
+  useEffect(() => {
+    let handle: number;
+    let lastFrame: number = 0;
+
+    function animationLoop(now: number) {
+      const delta = (now - lastFrame) / 1000;
+      lastFrame = now;
+      callback(delta);
+      handle = requestAnimationFrame(animationLoop);
+    }
+    handle = requestAnimationFrame(animationLoop);
+
+    return () => {
+      cancelAnimationFrame(handle);
+    };
+  }, [callback]);
+}
