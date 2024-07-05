634aee72ab70b880192fc37601ad433de88b03c1
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 06:50:47 -0300

    fix(alpaca-layout): Add portal for SideMenu component

--- src/features/alpaca-layout/index.tsx
+++ src/features/alpaca-layout/index.tsx
@@ -1,19 +1,25 @@
 import { ReactNode } from 'react';
 import { Alpaca } from '@/features/alpaca-layout/components/alpaca';
 import { Title } from '@/features/alpaca-layout/components/title';
-import { Navigation } from '@/features/alpaca-layout/components/navigation';
+import { Navigation } from '@/features/alpaca-layout/navigation';
 import styles from '@/features/alpaca-layout/index.module.sass';
 
 export function AlpacaLayout({ children }: { children: ReactNode }) {
   return (
-    <div className={styles.root}>
-      <header className={styles.header}>
-        <Title className={styles.title} />
-        <Alpaca className={styles.alpaca} />
-      </header>
-      <hr className={styles.divider} />
-      <Navigation />
-      {children}
-    </div>
+    <>
+      {/* Target for the SideMenu component's portal. */}
+      <div id="side-menu"></div>
+
+      {/* Root of all content. */}
+      <div className={styles.root}>
+        <header className={styles.header}>
+          <Title className={styles.title} />
+          <Alpaca className={styles.alpaca} />
+        </header>
+        <hr className={styles.divider} />
+        <Navigation />
+        {children}
+      </div>
+    </>
   );
 }
