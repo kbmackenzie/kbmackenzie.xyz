a3ad1cbf9352974357cc8721a495dc7462aa7da9
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 05:20:33 -0300

    refactor(alpaca-layout): Rename Button component to Buttons

--- src/features/alpaca-layout/components/buttons/index.tsx
+++ src/features/alpaca-layout/components/buttons/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { Fira_Mono } from 'next/font/google';
 import { ButtonData } from '@/features/alpaca-layout/types/button-data';
-import styles from '@/features/alpaca-layout/components/button/index.module.sass';
+import styles from '@/features/alpaca-layout/components/buttons/index.module.sass';
 
 const firaMono = Fira_Mono({
   weight:  ['400', '500', '700'],
@@ -13,7 +13,7 @@ function Button({ data }: { data: ButtonData }) {
   return <Link className={styles[type]} href={url}>{text}</Link>;
 }
 
-export function NavigationButtons({ buttons }: { buttons: ButtonData[] }) {
+export function Buttons({ buttons }: { buttons: ButtonData[] }) {
   const classes = [firaMono.className, styles.button].join(' ');
   return (
     <nav className={classes}>
