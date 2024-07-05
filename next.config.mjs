a5c2a8975a374ce5cd18d5278883ba08c65ce0e5
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 03:19:49 -0300

    chore: Add @style path alias in Webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = dirname(__filename)
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@styles'] = resolve(__dirname, 'src/styles');
+    return config;
+  },
+};
 
 export default nextConfig;
