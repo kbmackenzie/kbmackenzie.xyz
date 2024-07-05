61b752553e7c4e72266a4222586afda87e415d80
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 04:23:28 -0300

    feat: Begin work on 'alpaca-layout' feature

---
+++ src/features/alpaca-layout/index.tsx
@@ -0,0 +1,14 @@
+import styles from '@/features/alpaca-layout/index.module.sass';
+
+export function AlpacaLayout() {
+  return (<>
+    <header>
+    </header>
+
+    <nav>
+    </nav>
+
+    <main>
+    </main>
+  </>);
+}
