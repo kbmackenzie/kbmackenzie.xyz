1db623168ae49a090ece803b809c503869a758d5
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-05 06:23:04 -0300

    feat(alpaca-layout): Add root component, add styling

--- src/features/alpaca-layout/index.tsx
+++ src/features/alpaca-layout/index.tsx
@@ -1,14 +1,19 @@
+import { ReactNode } from 'react';
+import { Alpaca } from '@/features/alpaca-layout/components/alpaca';
+import { Title } from '@/features/alpaca-layout/components/title';
+import { Navigation } from '@/features/alpaca-layout/components/navigation';
 import styles from '@/features/alpaca-layout/index.module.sass';
 
-export function AlpacaLayout() {
-  return (<>
-    <header>
-    </header>
-
-    <nav>
-    </nav>
-
-    <main>
-    </main>
-  </>);
+export function AlpacaLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={styles.root}>
+      <header className={styles.header}>
+        <Alpaca className={styles.alpaca} />
+        <Title className={styles.title} />
+      </header>
+      <hr className={styles.divider} />
+      <Navigation />
+      {children}
+    </div>
+  );
 }
