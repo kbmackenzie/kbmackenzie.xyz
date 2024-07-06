785ade9490f60ca1020a07ecb510388d30341c59
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-06 09:16:35 -0300

    feat(home): Add 'about me' section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 //import Image from "next/image";
-//import styles from "./page.module.sass";
 import { AlpacaLayout } from '@/features/alpaca-layout';
+import { AboutMe } from '@/features/about-me';
+import { SkillHoneycomb } from '@/features/skill-honeycomb';
+import styles from '@/app/page.module.sass';
 
 export default function Home() {
   return (
     <AlpacaLayout>
       <main>
-        <h1>hello world!</h1>
+        <div className={styles.focus}>
+          <section className={styles.about}>
+            <AboutMe />
+          </section>
+          <aside className={styles.skills}>
+            <SkillHoneycomb />
+          </aside>
+        </div>
       </main>
     </AlpacaLayout>
   );
