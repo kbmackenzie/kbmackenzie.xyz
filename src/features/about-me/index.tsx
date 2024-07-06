8c3e7963a587a8b860a4a5329c8d7e13467baf70
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-06 09:15:48 -0300

    refactor(about-me): Greatly simplify component, improve styling

--- src/features/about-me/index.tsx
+++ src/features/about-me/index.tsx
@@ -1,13 +1,17 @@
-import { Introduction } from '@/features/about-me/components/introduction';
+import ReactMarkdown from 'react-markdown';
 import { RoundIcons } from '@/components/round-icons';
 import { socialMedia } from '@/features/about-me/store/social-media';
+import { styleClasses } from '@/utils/style-classes';
 import styles from '@/features/about-me/index.module.sass';
 
-export function AboutMe() {
+export function AboutMe({ className }: { className?: string }) {
   return (
-    <>
-      <Introduction className={styles.greeting} />
+    <div className={styleClasses(styles.greeting, className)}>
+      <h1>{`Hello, I'm Kelly!`}</h1>
+      <hr />
+      <ReactMarkdown>{`I'm a **full-stack web developer**, **language tinkerer** and cat enthusiast. I'm in love with **functional programming**, **compilers**, **DSLs** and **game development**.`}</ReactMarkdown>
+      <hr />
       <RoundIcons icons={socialMedia} className={styles.socials} />
-    </>
+    </div>
   );
 }
