da78b860e4a75d30c04b9898168c70f8a3a0882c
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-06 04:21:33 -0300

    feat(skill-honeycomb): Add skill constants

--- src/features/skill-honeycomb/store/skills.ts
+++ src/features/skill-honeycomb/store/skills.ts
@@ -1,5 +1,29 @@
 import { HoneycombSlot, HoneycombIcon } from '@/components/honeycomb-grid';
 
-/* todo: add skills */
+/* todo: add center icon */
 export const skills: ReadonlyMap<HoneycombSlot, HoneycombIcon> = new Map([
+  ['top'          , {
+    alt: 'haskell',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/haskell/haskell-plain.svg',
+  }],
+  ['top-right'    , {
+    alt: 'linux',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-plain.svg',
+  }],
+  ['bottom-right' , {
+    alt: 'neovim',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/neovim/neovim-plain.svg',
+  }],
+  ['bottom'       , {
+    alt: 'csharp',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-plain.svg',
+  }],
+  ['bottom-left'  , {
+    alt: 'react',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+  }],
+  ['top-left'     , {
+    alt: 'typescript',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg',
+  }],
 ]);
