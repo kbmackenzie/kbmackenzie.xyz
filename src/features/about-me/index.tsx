bd2f700d8157e07ce12a991e6f4dc3017079ff2e
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-07 08:17:21 -0300

    refactor(about-me): Use h2 instead of h1

--- src/features/about-me/index.tsx
+++ src/features/about-me/index.tsx
@@ -7,7 +7,7 @@ import styles from '@/features/about-me/index.module.sass';
 export function AboutMe({ className }: { className?: string }) {
   return (
     <div className={styleClasses(styles.greeting, className)}>
-      <h1>{`Hello, I'm Kelly!`}</h1>
+      <h2>{`Hello, I'm Kelly!`}</h2>
       <hr />
       <ReactMarkdown>{`I'm a **full-stack web developer**, **language tinkerer** and cat enthusiast. I'm in love with **functional programming**, **compilers**, **DSLs** and **game development**.`}</ReactMarkdown>
       <hr />
