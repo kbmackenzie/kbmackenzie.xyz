0d7bfdb5f648a264f8a066dee965a720836eb7b0
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-10 05:45:55 -0300

    feat(app): Add ProjectShowcase component to root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,24 @@
 import { AlpacaLayout } from '@/features/alpaca-layout';
 import { AboutMe } from '@/features/about-me';
 import { SkillHoneycomb } from '@/features/skill-honeycomb';
+import { ProjectShowcase } from '@/features/project-showcase';
 import styles from '@/app/page.module.sass';
 
 export default function Home() {
   return (
     <AlpacaLayout>
       <main>
-        <div className={styles.focus}>
+        <section className={styles.focus}>
           <section className={styles.about}>
             <AboutMe />
           </section>
           <aside className={styles.skills}>
             <SkillHoneycomb />
           </aside>
-        </div>
+        </section>
+        <section>
+          <ProjectShowcase />
+        </section>
       </main>
     </AlpacaLayout>
   );
