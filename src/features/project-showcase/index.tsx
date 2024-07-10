e60689bdab57b4a2419409705249e7d3b34c8584
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-10 05:33:08 -0300

    feat(project-showcase): Add better structure + styling for base component

--- src/features/project-showcase/index.tsx
+++ src/features/project-showcase/index.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { ProjectCard } from '@/features/project-showcase/components/project-card';
 import { projects } from '@/features/project-showcase/store/projects';
@@ -8,20 +11,31 @@ import arrowLeft from '@/features/project-showcase/assets/arrow-left.svg';
 import arrowRight from '@/features/project-showcase/assets/arrow-right.svg';
 
 export function ProjectShowcase({ className }: { className?: string }) {
+  const listRef = useRef<HTMLUListElement | null>(null);
+
+  function scroll(amount: number): void {
+    if (!listRef.current) return;
+    listRef.current.scrollBy(amount, 0);
+  }
+
   return (
-    <>
+    <div className={styleClasses(styles.showcase, className)}>
       <h2 className={styles.title}>Projects</h2>
-      <ul className={styleClasses(styles.showcase, className)}>
-        {projects.map(project => (
-          <li key={project.name}>
-            <ProjectCard className={styles.card} project={project} />
-          </li>
-        ))}
-      </ul>
-      <nav className={styles.arrows}>
-        <Arrow><Image src={arrowLeft}  alt="previous" /></Arrow>
-        <Arrow><Image src={arrowRight} alt="next"     /></Arrow>
-      </nav>
-    </>
+      <div className={styles.content}>
+        <ul ref={listRef} className={styles.projects}>
+          {projects.map(project => (
+            <li key={project.name}>
+              <ProjectCard className={styles.card} project={project} />
+            </li>
+          ))}
+        </ul>
+        <Arrow className={styles.prev} onClick={() => scroll(-20)}>
+          <Image src={arrowLeft} alt="previous" />
+        </Arrow>
+        <Arrow className={styles.next} onClick={() => scroll(20)}>
+          <Image src={arrowRight} alt="next" />
+        </Arrow>
+      </div>
+    </div>
   );
 }
