bdb4918465c027f0b1c156cd38995a9abb1a01ce
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-13 18:18:55 -0300

    refactor(latest-posts): A little clean-up

--- src/features/latest-posts/index.tsx
+++ src/features/latest-posts/index.tsx
@@ -1,8 +1,12 @@
-import { BrowsePosts } from '@/features/browse-posts';
+import { BrowsePosts, PostPredicate } from '@/features/browse-posts';
 import { styleClasses } from '@/utils/style-classes';
 import styles from '@/features/latest-posts/index.module.sass';
 
 export function LatestPosts({ className }: { className?: string; }) {
+  const predicate: PostPredicate = posts => posts
+    .sort((a, b) => a.timestamp - b.timestamp)
+    .slice(0, 10);
+
   return (
     <section className={styleClasses(styles.latest, className)}>
       <h2 className={styles.title}>
@@ -11,10 +15,7 @@ export function LatestPosts({ className }: { className?: string; }) {
       <hr className={styles.divider} />
       <BrowsePosts
         className={styles.posts}
-        predicate={posts => posts
-          .sort((a, b) => a.timestamp - b.timestamp)
-          .slice(0, 10)
-        } />
+        predicate={predicate} />
     </section>
   );
 
