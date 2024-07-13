80e975a89c3fe663137d8101bd34bff4061f0eec
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-13 03:37:02 -0300

    feat(latests-posts): Add post links, add link generation logic

--- src/features/latest-posts/index.tsx
+++ src/features/latest-posts/index.tsx
@@ -1,5 +1,6 @@
 import { PostMetadata } from '@/blog/blog-post';
 import { styleClasses } from '@/utils/style-classes';
+import Link from 'next/link';
 import styles from '@/features/latest-posts/index.module.sass';
 
 const dummyPosts: PostMetadata[] = [
@@ -22,6 +23,10 @@ const dummyPosts: PostMetadata[] = [
 export function LatestPosts({ className }: { className?: string; }) {
   const currentYear = new Date().getFullYear();
 
+  function linkToPost(date: Date, id: string): string {
+    return `/blog/${date.getFullYear()}/${id}`;
+  }
+
   return (
     <section className={styleClasses(styles.latest, className)}>
       <h2 className={styles.title}>
@@ -30,10 +35,12 @@ export function LatestPosts({ className }: { className?: string; }) {
       <hr className={styles.divider} />
       <ul className={styles.posts}>
         {dummyPosts.map(post => (
-          <li key={post.id} className={styles.post}>
-            <h3 className={styles.name}>{post.title}</h3>
-            <p className={styles.date}>{post.date.toLocaleDateString()}</p>
-            <p className={styles.description}>{post.description}</p>
+          <li key={post.id}>
+            <Link className={styles.post} href={linkToPost(post.date, post.id)}>
+              <h3 className={styles.name}>{post.title}</h3>
+              <p className={styles.date}>{post.date.toLocaleDateString()}</p>
+              <p className={styles.description}>{post.description}</p>
+            </Link>
           </li>
         ))}
       </ul>
