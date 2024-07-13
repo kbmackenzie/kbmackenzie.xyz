c5eac2c5fdab9d22eb6e1a7e7f2ee3afab301747
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-13 18:13:35 -0300

    refactor(latest-posts): Use BrowsePosts component instead of rewriting logic

--- src/features/latest-posts/index.tsx
+++ src/features/latest-posts/index.tsx
@@ -1,39 +1,20 @@
-import { fetchPostMetadata } from '@/blog/fetch-post';
+import { BrowsePosts } from '@/features/browse-posts';
 import { styleClasses } from '@/utils/style-classes';
-import Link from 'next/link';
 import styles from '@/features/latest-posts/index.module.sass';
 
-export async function LatestPosts({ className }: { className?: string; }) {
-  const metadata = await fetchPostMetadata();
-
-  const posts = metadata
-    .sort((a, b) => a.timestamp - b.timestamp)
-    .slice(0, 6);
-
-  function linkToPost(date: Date, id: string): string {
-    return `/blog/${date.getFullYear()}/${id}`;
-  }
-
+export function LatestPosts({ className }: { className?: string; }) {
   return (
     <section className={styleClasses(styles.latest, className)}>
       <h2 className={styles.title}>
         Latest Posts
       </h2>
       <hr className={styles.divider} />
-      <ul className={styles.posts}>
-        {posts.map(post => {
-          const date = new Date(post.timestamp);
-          return (
-            <li key={post.id}>
-              <Link className={styles.post} href={linkToPost(date, post.id)}>
-                <h3 className={styles.name}>{post.title}</h3>
-                <p className={styles.date}>{date.toLocaleDateString()}</p>
-                <p className={styles.description}>{post.description}</p>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <BrowsePosts
+        className={styles.posts}
+        predicate={posts => posts
+          .sort((a, b) => a.timestamp - b.timestamp)
+          .slice(0, 10)
+        } />
     </section>
   );
 
