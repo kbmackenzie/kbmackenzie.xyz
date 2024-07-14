8e515cf9e011493e6189f211a2864a14aa2cb8d3
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-14 04:42:13 -0300

    feat(utils): Add 'groupBy' list util

---
+++ src/utils/group-by.ts
@@ -0,0 +1,13 @@
+export function groupyBy<T1, T2>(array: T1[], groupOf: (t: T1) => T2): Map<T2, T1[]> {
+  const groups: Map<T2, T1[]> = new Map();
+
+  for (const value of array) {
+    const group = groupOf(value);
+    if (!groups.has(group)) {
+      groups.set(group, []);
+    }
+    groups.get(group)!.push(value);
+  }
+
+  return groups;
+}
