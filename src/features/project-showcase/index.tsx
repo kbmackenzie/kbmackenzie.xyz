0f366d9c564ea2f3fb26639f2b7ad98672aaedfd
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-14 18:14:54 -0300

    feat(project-showcase): Add slide-in animation for project cards

--- src/features/project-showcase/index.tsx
+++ src/features/project-showcase/index.tsx
@@ -2,15 +2,22 @@ import { ProjectCard } from '@/features/project-showcase/components/project-card
 import { projects } from '@/features/project-showcase/store/projects';
 import { styleClasses } from '@/utils/style-classes';
 import styles from '@/features/project-showcase/index.module.sass';
+import {CSSProperties} from 'react';
 
 export function ProjectShowcase({ className }: { className?: string }) {
   return (
     <ul className={styleClasses(styles.projects, className)}>
-      {projects.map(project => (
-        <li key={project.name}>
-          <ProjectCard className={styles.card} project={project} />
-        </li>
-      ))}
+      {projects.map((project, i) => {
+        const delay = 0.25 + (i * 0.15);
+        const style: CSSProperties = {
+          animationDelay: `${delay}s`
+        };
+        return (
+          <li key={project.name} className={styles.container} style={style}>
+            <ProjectCard className={styles.card} project={project} />
+          </li>
+        );
+      })}
     </ul>
   );
 }
