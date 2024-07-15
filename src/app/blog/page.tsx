596aa36aed2d9affdd42b1c0bd8a0ff8d0c0ecc3
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-15 03:04:26 -0300

    refactor(app): In /blog, prefer to use yearOfPost over re-writing util

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,13 @@
 import { fetchPostMetadata } from '@/blog/fetch-post';
 import { groupyBy } from '@/utils/group-by';
+import { yearOfPost } from '@/blog/blog-post';
 import { BrowsePosts } from '@/features/browse-posts';
 import styles from '@/app/blog/page.module.sass';
 
 export default async function Blog() {
   const postsByYear = groupyBy(
     await fetchPostMetadata(),
-    post => new Date(post.timestamp).getFullYear()
+    post => yearOfPost(post),
   );
 
   return (
