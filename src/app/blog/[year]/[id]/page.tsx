948ae30f48ebd3714a31353fd66abd11976f083d
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-15 03:53:26 -0300

    feat(app): Add dynamic post fetching logic + 'Not found' page

--- src/app/blog/[year]/[id]/page.tsx
+++ src/app/blog/[year]/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { PostQuery, yearOfPost, isQueriedPost, BlogPost } from '@/blog/blog-post';
 import { fetchPost, fetchPostMetadata } from '@/blog/fetch-post';
+import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
+import styles from '@/app/blog/[year]/[id]/page.module.sass';
 
 type PostParams = {
   year: string;
@@ -8,16 +11,40 @@ type PostParams = {
 
 export async function generateStaticParams(): Promise<PostParams[]> {
   const metadata = await fetchPostMetadata();
-
-  return metadata.map(data => ({
-    year: new Date(data.timestamp).getFullYear().toString(),
-    id:   data.id,
+  return metadata.map(post => ({
+    year: yearOfPost(post).toString(),
+    id:   post.id,
   }));
 }
 
+function NotFound() {
+  return (
+    <main className={styles.error}>
+      <h2>Post not found.</h2>
+      <Link href="/blog">See All Posts</Link>
+    </main>
+  );
+}
+
 export default async function Post({ params }: { params: PostParams }) {
-  const post = await fetchPost(params.year, params.id);
-  /* todo: post rendering logic */
+  const metadata = await fetchPostMetadata();
+  const query: PostQuery = {
+    year: Number(params.year),
+    id: params.id,
+  };
+
+  if (!metadata.some(post => isQueriedPost(query, post))) {
+    return <NotFound />;
+  }
+
+  let post: BlogPost;
+  try {
+    post = await fetchPost(query);
+  }
+  catch (error) {
+    return <NotFound />;
+  }
+
   return (
     <>
       <h2>{post.metadata.title}</h2>
