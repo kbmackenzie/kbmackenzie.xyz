2c518568344d018b449bcff27d82dc0290105e18
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-15 02:03:14 -0300

    feat(blog): Add PostQuery type, add blog post utils

--- src/blog/blog-post.ts
+++ src/blog/blog-post.ts
@@ -10,3 +10,16 @@ export type BlogPost = Readonly<{
   metadata: PostMetadata;
   body: string;
 }>;
+
+export type PostQuery = Readonly<{
+  year: number;
+  id: string;
+}>;
+
+export function yearOfPost(post: PostMetadata) {
+  return new Date(post.timestamp).getFullYear();
+}
+
+export function isQueriedPost(query: PostQuery, post: PostMetadata): boolean {
+  return yearOfPost(post) === query.year && post.id === query.id;
+}
