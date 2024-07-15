3e79b6db761ab4f48f27a1e313401ecfedbb52a4
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-15 03:52:07 -0300

    refactor(blog): Avoid premature optimization

--- src/blog/fetch-post.ts
+++ src/blog/fetch-post.ts
@@ -35,16 +35,9 @@ export async function fetchPost({ year, id }: PostQuery): Promise<BlogPost> {
   };
 }
 
-/* Cache I/O operation. (Static file never changes.) */
-let metadataCache: PostMetadata[] | null = null;
-
 export async function fetchPostMetadata(): Promise<PostMetadata[]> {
-  if (metadataCache !== null) return metadataCache;
-
   const metaPath = path.join(blogDirectory, 'posts.json');
   const contents = await readFile(metaPath);
   const metadata = JSON.parse(contents.toString()) as PostMetadata[];
-
-  metadataCache  = metadata;
   return metadata;
 }
