a8a5ce02f3869786b0869b7c3dd7788c075dde7e
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-21 08:47:05 -0300

    feat(app/blog): Handle whitespace in searches better; clear search box after input accepted

--- src/app/blog/search.tsx
+++ src/app/blog/search.tsx
@@ -5,10 +5,6 @@ import { AttrsAndStyle } from '@/types/attrs-and-style';
 import { useRouter, usePathname } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
 
-function isWhitespace(input: string) {
-  return /^\s*$/.test(input);
-}
-
 export function Search(props: AttrsAndStyle<HTMLDivElement>) {
   const input = useRef<HTMLInputElement | null>(null);
 
@@ -17,9 +13,15 @@ export function Search(props: AttrsAndStyle<HTMLDivElement>) {
   const searchParams = useSearchParams();
   
   const addTagParam = useCallback(
-    (tag: string) => {
+    (input: string) => {
       const params = new URLSearchParams(searchParams.toString());
-      params.set('tagged', tag);
+      const tag= input.trim();
+      if (tag !== '') {
+        params.set('tagged', tag);
+      }
+      else {
+        params.delete('tagged');
+      }
       return params.toString();
     },
     [searchParams]
@@ -27,11 +29,11 @@ export function Search(props: AttrsAndStyle<HTMLDivElement>) {
 
   const onKeyDown = useCallback(
     (event: KeyboardEvent<HTMLDivElement>) => {
-      if (event.repeat || event.key !== 'Enter' || event.shiftKey) return;
-      if (!input.current || isWhitespace(input.current.value)) return;
+      if (event.repeat || event.key !== 'Enter' || !input.current) return;
       event.preventDefault();
 
       const params = addTagParam(input.current.value);
+      input.current.value = '';
       router.push(`${pathname}?${params.toString()}`);
     },
     [input, router, pathname, addTagParam]
