f2833e1eb13ceaecee4f1c9fdfa25ecdc9c8e1d2
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-21 04:59:45 -0300

    feat(app/blog): Let posts be filtered by tag through search param

--- src/app/blog/posts.tsx
+++ src/app/blog/posts.tsx
@@ -5,13 +5,23 @@ import { groupyBy } from '@/utils/group-by';
 import { yearOfPost } from '@/blog/blog-post';
 import { BrowsePosts } from '@/features/browse-posts';
 import { Observer } from '@/components/observer';
+import { useSearchParams } from 'next/navigation';
 import styles from '@/app/blog/posts.module.sass';
 
 export function Posts({ posts }: { posts: PostMetadata[] }) {
+  const searchParams = useSearchParams();
+  const tag = searchParams.get('tagged');
+
+  function hasTag(post: PostMetadata): boolean {
+    if (tag === null || tag === '') return true;
+    return post.tags.includes(tag);
+  }
+
   const postsByYear = groupyBy(
-    posts,
+    posts.filter(hasTag),
     post => yearOfPost(post),
   );
+
   return (
     <ul className={styles.list}>
       {postsByYear.map(([year, posts]) => (
