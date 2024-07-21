6ccaa04c672c6f9e3436dbd17ae7c9a3db8adf2d
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-21 09:02:24 -0300

    refactor(app/blog): A little clean-up; remove unused stylesheet

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,11 @@
 import { fetchPostMetadata } from '@/blog/fetch-post';
 import { Posts } from '@/app/blog/posts';
 import { Suspense } from 'react';
-import styles from '@/app/blog/page.module.sass';
 
 export default async function Blog() {
   const posts = await fetchPostMetadata();
-
   return (
-    <main className={styles.blog}>
+    <main>
       <Suspense>
         <Posts posts={posts} />
       </Suspense>
