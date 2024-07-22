b93065f6e0ed15992d9b204bb2b24c4277ce3401
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-22 19:03:12 -0300

    refactor(about-me): Write description with JSX instead of markdown

--- src/features/about-me/index.tsx
+++ src/features/about-me/index.tsx
@@ -1,15 +1,20 @@
-import ReactMarkdown from 'react-markdown';
 import { RoundIcons } from '@/components/round-icons';
 import { socialMedia } from '@/features/about-me/store/social-media';
 import { styleClasses } from '@/utils/style-classes';
 import styles from '@/features/about-me/index.module.sass';
+
+function Introduction() {
+  return (
+    <p>I'm a <strong>full-stack web developer</strong>, <strong>language tinkerer</strong> and cat enthusiast. I'm in love with <strong>functional programming</strong>, <strong>compilers</strong>, <strong>DSLs</strong> and <strong>game development</strong>.</p>
+  );
+}
 
 export function AboutMe({ className }: { className?: string }) {
   return (
     <section className={styleClasses(styles.greeting, className)}>
       <h2>{`Hello, I'm Kelly!`}</h2>
       <hr />
-      <ReactMarkdown>{`I'm a **full-stack web developer**, **language tinkerer** and cat enthusiast. I'm in love with **functional programming**, **compilers**, **DSLs** and **game development**.`}</ReactMarkdown>
+      <Introduction />
       <hr />
       <RoundIcons icons={socialMedia} className={styles.socials} />
     </section>
