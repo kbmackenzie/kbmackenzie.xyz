373a69c6031527e3eeffe7d0e0e4d4eddbb13ae3
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-24 09:16:32 -0300

    feat(components): Add support for default highlighters in MarkdownHighlight

--- src/components/markdown-highlight/index.tsx
+++ src/components/markdown-highlight/index.tsx
@@ -1,9 +1,17 @@
 import { HTMLAttributes } from 'react';
+import hljs from 'highlight.js/lib/core';
+import { supportedLanguages } from '@/store/highlight';
 import ReactMarkdown, { Components, ExtraProps } from 'react-markdown';
 import { styleClasses } from '@/utils/style-classes';
 
 /* Markdown, with syntax highlighting.
- * Accepts syntax highlighters based on language. */
+ * Accepts syntax highlighters based on language.
+ *
+ * Note to future self: This will only be used for blog posts, *never* user input.
+ * Blog posts are static and come from the server.
+ * Thus, blog post input *can* be trusted, and using 'dangerouslySetInnerHTML' is fine.
+ *
+ * todo: 'purify' HTML for future me's sanity. */
 
 export type Highlighter = (
   input: string,
@@ -13,21 +21,37 @@ export type Highlighter = (
 type Props = {
   className?: string;
   children: string;
-  highlighters?: Map<string, Highlighter>;
+  customHighlighters?: Map<string, Highlighter>;
 };
 
 const languageName: RegExp = /language-(\w+)/;
 
-export function MarkdownHighlight({ className, children, highlighters }: Props) {
-  const components: Components = {
-    code(props) {
-      const language = languageName.exec(props.className || '')?.[1];
+const defaultHighlighters = new Map<string, Highlighter>(
+  supportedLanguages.map(language => [language, (input, props) => {
+    const html = hljs.highlight(input, { language: language }).value;
+    return <code {...props} dangerouslySetInnerHTML={{ __html: html }}></code>
+  }])
+);
 
-      if (!language || !highlighters || !highlighters.has(language)) {
-        return <code {...props}></code>
+export function MarkdownHighlight({ className, children, customHighlighters }: Props) {
+  function getHighlighter(language: string): Highlighter | null {
+    if (customHighlighters && customHighlighters.has(language)) {
+      return customHighlighters.get(language)!;
+    }
+    if (defaultHighlighters.has(language)) {
+      return defaultHighlighters.get(language)!;
+    }
+    return null;
+  }
+
+  const components: Components = {
+    code({ children, ...props }) {
+      const language = languageName.exec(props.className || '')?.[1];
+      if (!language || !getHighlighter(language)) {
+        return <code {...props}>{children}</code>
       }
-      const highlighter = highlighters.get(language)!;
-      const input = String(props.children);
+      const highlighter = getHighlighter(language)!;
+      const input = String(children);
 
       return highlighter(input, props);
     },
