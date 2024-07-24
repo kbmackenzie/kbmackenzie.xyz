23c611c97991590e2374883d8a60fb4f7116ccec
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-24 18:58:40 -0300

    feat(app/blog): Add slide-in transition

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,12 @@
 import { fetchPostMetadata } from '@/blog/fetch-post';
 import { Posts } from '@/app/blog/posts';
 import { Suspense } from 'react';
+import styles from '@/app/blog/page.module.sass';
 
 export default async function Blog() {
   const posts = await fetchPostMetadata();
   return (
-    <main>
+    <main className={styles.blog}>
       <Suspense>
         <Posts posts={posts} />
       </Suspense>
