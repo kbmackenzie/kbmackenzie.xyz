0d8efaf70f51609c623086fa2827a10033c478b3
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-26 09:42:15 -0300

    feat(app/project): Add page title + description

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,18 @@
 import { ProjectCard } from '@/app/projects/project-card';
 import { projects } from '@/features/projects';
-import { styleClasses } from '@/utils/style-classes';
+import { Markdown } from '@/components/markdown';
 import styles from '@/app/projects/page.module.sass';
+
+const description: string = 'A list of my favorite projects I have worked on, including: **esoteric programming languages**; little **games**; **mods** and **modding tools**; and anything else that piques my interest.';
 
 export default function Projects() {
   return (
     <main className={styles.projects}>
+      <h2 className={styles.title}>Projects</h2>
+      <Markdown className={styles.description}>
+        {description}
+      </Markdown>
+      <hr className={styles.divider} />
       <ul className={styles.list}>
         {projects.map(project => (
           <li key={project.name}>
