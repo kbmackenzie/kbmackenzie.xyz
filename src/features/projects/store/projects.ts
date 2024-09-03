2b141ba20c8e7da0107dafdf32329752b01de57a
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-03 08:08:32 -0300

    refactor(projects): Get project datafile paths with 'getStaticFile'

--- src/features/projects/store/projects.ts
+++ src/features/projects/store/projects.ts
@@ -3,15 +3,10 @@ import { Skill } from '@/types/skill';
 import mewlixLogo from '@/features/projects/assets/mewlix.png';
 import alpacaLogo from '@/features/projects/assets/alpaca.png';
 import terminalIcon from '@/features/projects/assets/terminal.png';
-import url from 'url';
-import path from 'path'
+import { getStaticFile } from '@/utils/static-file';
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename)
-const projectDirectory = path.resolve(__dirname, '../../../../static/projects/');
-
-function getDatafile(datafile: string): string {
-  return path.join(projectDirectory, datafile);
+function getProjectDatafile(datafile: string): string {
+  return getStaticFile('projects', datafile);
 }
 
 function joinLines(...lines: string[]): string {
@@ -30,7 +25,7 @@ export const projects: Project[] = [
       src: mewlixLogo,
       alt: 'mewlix logo',
     },
-    datafile: getDatafile('mewlix.md'),
+    datafile: getProjectDatafile('mewlix.md'),
     url: 'https://github.com/kbmackenzie/mewlix',
     skills: [Skill.Haskell, Skill.TypeScript, Skill.HTML, Skill.Sass],
   },
@@ -45,7 +40,7 @@ export const projects: Project[] = [
       src: alpacaLogo,
       alt: 'kbmackenzie.xyz logo',
     },
-    datafile: getDatafile('kbmackenzie-xyz.md'),
+    datafile: getProjectDatafile('kbmackenzie-xyz.md'),
     url: 'https://github.com/kbmackenzie/kbmackenzie.xyz',
     skills: [Skill.TypeScript, Skill.React, Skill.Sass, Skill.NextJS],
   },
@@ -57,7 +52,7 @@ export const projects: Project[] = [
       src: terminalIcon,
       alt: 'terminal icon',
     },
-    datafile: getDatafile('mods/inscryption.md'),
+    datafile: getProjectDatafile('mods/inscryption.md'),
     url: 'https://thunderstore.io/c/inscryption/p/KellyBetty/',
     skills: [Skill.CSharp, Skill.Unity],
   },
@@ -69,7 +64,7 @@ export const projects: Project[] = [
       src: terminalIcon,
       alt: 'terminal icon',
     },
-    datafile: getDatafile('mods/cult-of-the-lamb.md'),
+    datafile: getProjectDatafile('mods/cult-of-the-lamb.md'),
     url: 'https://thunderstore.io/c/cult-of-the-lamb/p/KellyBetty/',
     skills: [Skill.CSharp, Skill.Unity],
   },
