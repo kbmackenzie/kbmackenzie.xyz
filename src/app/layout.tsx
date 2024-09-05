6e03576714c3ef5b0e01bdaf0f8bd7b3716422ed
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-05 06:08:00 -0300

    refactor(app): Avoid double-importing font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from 'next';
-import { Open_Sans } from 'next/font/google';
+import { openSans } from '@/fonts';
 import { AlpacaLayout } from '@/features/alpaca-layout';
 import './globals.sass';
-
-const openSans = Open_Sans({
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'kelly\'s hut',
