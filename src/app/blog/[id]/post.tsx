0547c2c47a441ca0294f2cbb31f747675bf389c8
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-06 04:49:39 -0300

    feat(app/blog): Add better post animations

--- src/app/blog/[id]/post.tsx
+++ src/app/blog/[id]/post.tsx
@@ -35,7 +35,7 @@ export function Post({ post, className }: Props) {
         </p>
       </div>
       <hr className={styles.divider} />
-      <MarkdownStylish className="alpaca-markdown">
+      <MarkdownStylish className={styleClasses('alpaca-markdown', styles.body)}>
         {post.body}
       </MarkdownStylish>
       <hr className={styles.divider} />
