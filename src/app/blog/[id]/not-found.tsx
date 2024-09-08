9da55486b7affe9f3e73a0d92cf6f1e064caf814
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-08 04:45:11 -0300

    feat(app/blog): For 'not-found' component, wrap AlpacaNotFound in <main>

--- src/app/blog/[id]/not-found.tsx
+++ src/app/blog/[id]/not-found.tsx
@@ -2,6 +2,8 @@ import { AlpacaNotFound } from '@/features/alpaca-not-found';
 
 export default function NotFound() {
   return (
-    <AlpacaNotFound info="Post not found!" />
+    <main>
+      <AlpacaNotFound info="Post not found!" />
+    </main>
   );
 }
