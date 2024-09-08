b7c9ddf4c9b309103323fe37fe35e534efbcf99b
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-08 04:45:37 -0300

    feat(app/projects): For 'not-found' component, wrap AlpacaNotFound in <main>

--- src/app/projects/mewlix/[doc]/not-found.tsx
+++ src/app/projects/mewlix/[doc]/not-found.tsx
@@ -2,6 +2,8 @@ import { AlpacaNotFound } from '@/features/alpaca-not-found';
 
 export default function NotFound() {
   return (
-    <AlpacaNotFound info="No documentation page matches the request!" />
+    <main>
+      <AlpacaNotFound info="No documentation page matches the request!" />
+    </main>
   );
 }
