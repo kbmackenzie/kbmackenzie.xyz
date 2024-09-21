e35e06bbd99b00bd43965514a7fd8b6b07aa2776
kbmackenzie <kelly.a.betty@gmail.com>
2024-09-21 02:03:23 -0300

    refactor(components): Add cleaner anchor logic for MarkdownStylish

--- src/components/markdown-stylish/anchor.tsx
+++ src/components/markdown-stylish/anchor.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from 'react';
+import { ComponentProps, ReactNode, createElement } from 'react';
 import { Components } from 'react-markdown';
 import { onlyText } from 'react-children-utilities';
 
 type Headings = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+type HeadingProps = ComponentProps<Headings>;
 type HeadingComponent = Components[Headings];
 
 function getId(children: ReactNode): string {
@@ -10,32 +11,24 @@ function getId(children: ReactNode): string {
   return text.replace(/\W/g, '-').toLowerCase();
 }
 
-/* A little repetitive, but automating this would be extremely pointless.
- * There are only 6 headings; a little repetition is fine. */
+function headingAnchor(heading: Headings): HeadingComponent {
+  return function({ children, ...props }) {
+    const id = getId(children);
+    const newProps: HeadingProps = {
+      id: id,
+      ...props
+    };
+    return createElement(heading, newProps, children);
+  }
+}
 
+/* A little repetitive, but there are only 6 headings anyway!
+ * No point in overcomplicating the little snippet below. */
 export const headingAnchors: Record<Headings, HeadingComponent> = {
-  h1({ children, ...props }) {
-    const id = getId(children);
-    return <h1 id={id} {...props}>{children}</h1>;
-  },
-  h2({ children, ...props }) {
-    const id = getId(children);
-    return <h2 id={id} {...props}>{children}</h2>;
-  },
-  h3({ children, ...props }) {
-    const id = getId(children);
-    return <h3 id={id} {...props}>{children}</h3>;
-  },
-  h4({ children, ...props }) {
-    const id = getId(children);
-    return <h4 id={id} {...props}>{children}</h4>;
-  },
-  h5({ children, ...props }) {
-    const id = getId(children);
-    return <h5 id={id} {...props}>{children}</h5>;
-  },
-  h6({ children, ...props }) {
-    const id = getId(children);
-    return <h6 id={id} {...props}>{children}</h6>;
-  },
+  'h1': headingAnchor('h1'),
+  'h2': headingAnchor('h2'),
+  'h3': headingAnchor('h3'),
+  'h4': headingAnchor('h4'),
+  'h5': headingAnchor('h5'),
+  'h6': headingAnchor('h6'),
 };
