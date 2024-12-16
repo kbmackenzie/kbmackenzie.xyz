a5ef06812ab43f4efe4111c7095f84901221f25d
kbmackenzie <kelly.a.betty@gmail.com>
2024-12-16 14:27:10 -0300

    fix(link-alias): Fix alias in table

--- src/features/link-alias/store/alias-table.ts
+++ src/features/link-alias/store/alias-table.ts
@@ -3,5 +3,5 @@ export type AliasTable = Record<string, string>;
 /* Aliases for URLs; primarily used when rendering our Markdown content.
  * Note: They must *ALWAYS* resolve to a RELATIVE PATH in the website. */
 export const aliasTable: AliasTable = {
-  '@mewlix': '/projects/mewlix',
+  '@mewlix': '/projects/mewlix/',
 };
