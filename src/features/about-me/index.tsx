9e558dda7e60217638ff3cf31f0b998115f97b66
kbmackenzie <kelly.a.betty@gmail.com>
2025-01-21 02:33:54 -0300

    feat(about-me): Add better introduction

--- src/features/about-me/index.tsx
+++ src/features/about-me/index.tsx
@@ -4,7 +4,7 @@ import { socialMedia } from '@/features/about-me/store/social-media';
 import { styleClasses } from '@/utils/style-classes';
 import styles from '@/features/about-me/index.module.sass';
 
-const introduction = `I'm a **software engineer**, **language tinkerer** and cat enthusiast. I'm in love with **functional programming**, **compilers**, **DSLs** and **game development**.`;
+const introduction = `I'm a **software developer** and language tinkerer. I adore esoteric languages, interpreters, compilers, DSLs and game development!`;
 
 export function AboutMe({ className }: { className?: string }) {
   return (
