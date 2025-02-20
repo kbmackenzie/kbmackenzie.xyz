67f32172fee8d081a02b95362d0f903162a69fb8
kbmackenzie <kelly.a.betty@gmail.com>
2025-02-20 14:07:45 -0300

    feat(project-showcase): Showcase only six projects

--- src/features/project-showcase/index.tsx
+++ src/features/project-showcase/index.tsx
@@ -1,13 +1,14 @@
-import { ProjectCard } from '@/features/project-showcase/components/project-card';
+import { CSSProperties } from 'react';
 import { projects } from '@/features/projects';
+import { ProjectCard } from '@/features/project-showcase/components/project-card';
 import { styleClasses } from '@/utils/style-classes';
 import styles from '@/features/project-showcase/index.module.sass';
-import { CSSProperties } from 'react';
 
+/* Showcase top projects. */
 export function ProjectShowcase({ className }: { className?: string }) {
   return (
     <ul className={styleClasses(styles.projects, className)}>
-      {projects.map((project, i) => {
+      {projects.slice(0, 6).map((project, i) => {
         const delay = 0.25 + (i * 0.15);
         const style: CSSProperties = {
           animationDelay: `${delay}s`
