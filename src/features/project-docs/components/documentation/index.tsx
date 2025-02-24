976f952d60f9e3420e9283e2d061e69c922d3585
kbmackenzie <kelly.a.betty@gmail.com>
2025-02-24 13:57:58 -0300

    feat(project-docs): Add project icon

--- src/features/project-docs/components/documentation/index.tsx
+++ src/features/project-docs/components/documentation/index.tsx
@@ -1,6 +1,7 @@
-import { MarkdownStylish } from '@/components/markdown-stylish';
+import Image from 'next/image';
 import { ProjectDoc } from '@/features/project-docs/types/project-doc';
 import { SkillList } from '@/features/project-docs/components/skill-list';
+import { MarkdownStylish } from '@/components/markdown-stylish';
 import { styleClasses } from '@/utils/style-classes';
 import styles from '@/features/project-docs/components/documentation/index.module.sass';
 
@@ -11,14 +12,22 @@ type DocumentationProps = {
 
 export function Documentation({ project, className }: DocumentationProps) {
   return (
-    <article className={styleClasses(styles.doc, className)}>
-      <h3 className={styles.title}>
-        {project.name}
-      </h3>
-      <SkillList className={styles.skills} project={project} />
-      <MarkdownStylish>
-        {project.documentation}
-      </MarkdownStylish>
+    <article className={styleClasses(styles.container, className)}>
+      <Image
+        src={project.image.src}
+        alt={project.image.alt}
+        width={128}
+        height={128}
+        className={styles.icon} />
+      <div className={styles.documentation}>
+        <h3 className={styles.title}>
+          {project.name}
+        </h3>
+        <SkillList className={styles.skills} project={project} />
+        <MarkdownStylish>
+          {project.documentation}
+        </MarkdownStylish>
+      </div>
     </article>
   );
 }
