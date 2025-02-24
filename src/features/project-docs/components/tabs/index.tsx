caa1ee9cf43760c0c076015427f0758caaa57632
kbmackenzie <kelly.a.betty@gmail.com>
2025-02-24 15:54:08 -0300

    feat(project-docs): Add arrows functionality

--- src/features/project-docs/components/tabs/index.tsx
+++ src/features/project-docs/components/tabs/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { ProjectTab } from '@/features/project-docs/types/project-doc';
 import { styleClasses } from '@/utils/style-classes';
+import { clamp } from '@/utils/math';
 import styles from '@/features/project-docs/components/tabs/index.module.sass';
 
 type TabsProps = {
@@ -32,9 +33,19 @@ function Tab({ project, setCurrent, isCurrent }: TabProps) {
 }
 
 export function Tabs({ projects, setCurrent, current, className }: TabsProps) {
+  /* An O(n) solution, but it shouldn't matter. :)
+   * I could use a linked list, but I enjoy keeping it simple. */
+  function moveByOffset(offset: number): void {
+    const index    = projects.indexOf(current) + offset;
+    const selected = projects[clamp(index, 0, projects.length - 1)];
+    setCurrent(selected);
+  }
+
   return (
     <nav className={styleClasses(styles.container, className)}>
-      <button className={styleClasses(styles.arrow, styles.left)}>
+      <button
+        onClick={() => moveByOffset(-1)}
+        className={styleClasses(styles.arrow, styles.left)}>
         &lt;
       </button>
       <ul className={styles.tabs}>
@@ -47,7 +58,9 @@ export function Tabs({ projects, setCurrent, current, className }: TabsProps) {
           </li>
         ))}
       </ul>
-      <button className={styleClasses(styles.arrow, styles.right)}>
+      <button
+        onClick={() => moveByOffset(1)}
+        className={styleClasses(styles.arrow, styles.right)}>
         &gt;
       </button>
     </nav>
