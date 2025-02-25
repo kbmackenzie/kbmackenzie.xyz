af9cea9545ff3e2ab6fc94b455c49a026f5a79c9
kbmackenzie <kelly.a.betty@gmail.com>
2025-02-25 14:17:57 -0300

    feat(app): Let BubblegumButton generate its own title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
           <BrowsePosts
             posts={latest}
             className={styles.posts} />
-          <BubblegumButton href="/blog" title="See all posts">
+          <BubblegumButton href="/blog">
             See All Posts
           </BubblegumButton>
         </Observer>
