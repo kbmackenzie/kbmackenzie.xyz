75f096a7d66e1a1c7663c344c31cd9b15f95085b
kbmackenzie <kelly.a.betty@gmail.com>
2025-02-25 14:16:20 -0300

    feat(components): Generate title when not is given in BubblegumButton component

--- src/components/bubblegum-button/index.tsx
+++ src/components/bubblegum-button/index.tsx
@@ -1,19 +1,21 @@
 import { ReactNode } from 'react';
 import Link from 'next/link';
+import { onlyText } from 'react-children-utilities';
 import { styleClasses } from '@/utils/style-classes';
 import styles from '@/components/bubblegum-button/index.module.sass';
 
 type BubblegumButtonProps = {
   href: string;
-  title: string;
+  title?: string;
   className?: string;
   children: ReactNode
 };
 
 export function BubblegumButton({ href, title, children, className }: BubblegumButtonProps) {
+  const titleStr = title ?? onlyText(children);
   return (
-    <div className={styleClasses(styles.container, className)}>
-      <Link href={href} title={title} className={styles.button}>
+    <div title={titleStr} className={styleClasses(styles.container, className)}>
+      <Link href={href} className={styles.button}>
         {children}
       </Link>
     </div>
