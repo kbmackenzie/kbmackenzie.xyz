b80876babb5f9634ecf0ad74983e3e2a6a59e935
kbmackenzie <kelly.a.betty@gmail.com>
2025-02-24 21:23:11 -0300

    feat(project-docs): Select initial project based on query params

--- src/features/project-docs/components/navigation/index.tsx
+++ src/features/project-docs/components/navigation/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { ReadonlyURLSearchParams, useSearchParams } from 'next/navigation';
 import { ProjectTab } from '@/features/project-docs/types/project-doc';
 import { Tabs } from '@/features/project-docs/components/tabs';
 import { Documentation } from '@/features/project-docs/components/documentation';
@@ -12,11 +13,20 @@ type NavigationProps = {
   projects: ProjectTab[];
 };
 
+function queryProject(params: ReadonlyURLSearchParams, projects: ProjectTab[]): ProjectTab {
+  const id = params.get('project');
+  const selected = id && projects.find(project => project.id === id);
+  return selected || projects[0];
+}
+
 export function Navigation({ projects, className }: NavigationProps) {
-  const [current, setCurrent] = useState<ProjectTab>(projects[0]);
+  const searchParams = useSearchParams();
+  const [current, setCurrent] = useState<ProjectTab>(
+    queryProject(searchParams, projects)
+  );
   /* The plan of action is:
    * - Pass 'current' to Documentation component.
-   * - Pass 'setCurrent' to Tabs component. :) */
+   * - Pass 'current' and 'setCurrent' to Tabs component. :) */
   return (
     <div className={styleClasses(styles.navigation, className)}>
       <Tabs projects={projects} current={current} setCurrent={setCurrent} />
